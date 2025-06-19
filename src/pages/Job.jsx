aefb43bc798df7ff5dd6f58f7dbd380a3e4ce254
Dip290703 <dipanshu6233@gmail.com>
2025-06-19 15:19:33 +0530

    job portal

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { getJobs } from "../api/apiJobs";
 
 const Job = () => {
+  const { session, isLoaded } = useSession();
+
+  const fetchJobs = async () => {
+    if (!isLoaded || !session) {
+      console.log("🔁 Session not loaded yet.");
+      return;
+    }
+
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+
+    const data = await getJobs(supabaseAccessToken);
+    console.log("✅ Jobs fetched:", data);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, [isLoaded, session]);
+
   return <div>Job</div>;
 };
 
