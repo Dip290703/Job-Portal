2de6c665962fd9490fc1ff93a5632901a6d5c4ed
Dip290703 <dipanshu6233@gmail.com>
2025-06-29 11:06:03 +0530

    Deply

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { Button } from "./ui/button";
-import { SignedIn, SignedOut, SignIn, UserButton, useUser } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
 import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
 
 const Header = () => {
   const [showSignIn, setshowSignIn] = useState(false);
-  const [search, setSearch]=useSearchParams()
-  const {user}= useUser()
-  useEffect(()=>{
-  if(search.get("sign-in")){
-    setshowSignIn(true)
-  }
-  },[search])
-
+  const [search, setSearch] = useSearchParams();
+  const { user, isSignedIn } = useUser();
+  useEffect(() => {
+    if (search.get("sign-in")) {
+      setshowSignIn(true);
+    }
+  }, [search]);
 
   const handleOutLay = (e) => {
     if (e.target === e.currentTarget) {
       setshowSignIn(false);
-      setSearch({})
+      setSearch({});
     }
   };
   return (
@@ -28,28 +33,43 @@ const Header = () => {
           <img src="/logo (1).png" alt="logo" className="h-20" />
         </Link>
 
-        <div className="flex gap-8">
+        <div className="flex items-center gap-3 sm:gap-6">
+          
+            {isSignedIn && (
+              <div className="flex  gap-2 sm:gap-5">
+                <Link to="/savedjobs">
+                  <Heart />
+                </Link>
+                <Link to="/myjobs">
+                <BriefcaseBusiness />
+                </Link>
+              </div>
+            )}
+        
           <SignedOut>
             <Button variant="outline" onClick={() => setshowSignIn(true)}>
               Login
             </Button>
           </SignedOut>
           <SignedIn>
-           
-            {
-              user?.unsafeMetadata?.role === "Recruiter" &&
-              (<Link to="/postjobs"> <Button variant="destructive" className="rounded-full">
-              <PenBox size={20} className="mr-2" />
-              Post a Job
-            </Button></Link>)}
+            {user?.unsafeMetadata?.role === "Recruiter" && (
+              <Link to="/postjobs">
+                {" "}
+                <Button variant="destructive" className="rounded-full">
+                  <PenBox size={20} className="mr-2" />
+                  Post a Job
+                </Button>
+              </Link>
+            )}
+
             <UserButton
-            appearance={{
-              elements:{
-                avatarBox: "w-10 h-10 min-w-10 min-h-10 rounded-full",
-              }
-            }}
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10 min-w-10 min-h-10 rounded-full",
+                },
+              }}
             >
-              <UserButton.MenuItems>
+              {/* <UserButton.MenuItems>
                 <UserButton.Link
                   label="My Jobs"
                   labelIcon={<BriefcaseBusiness size={15} />}
@@ -60,7 +80,7 @@ const Header = () => {
                   labelIcon={<Heart size={15} />}
                   href="/savedjobs"
                 />
-              </UserButton.MenuItems>
+              </UserButton.MenuItems> */}
             </UserButton>
           </SignedIn>
         </div>
